19e303e3026d44f784879f6bb11b17040c383f0b
kundu <kunduachyut19@gmail.com>
2025-04-12 10:44:27 +0530

    12-04-2025 update

--- methodsJava.java
+++ methodsJava.java
@@ -4,11 +4,12 @@ public class methodsJava {
     }
 
      void printS(){
-        System.out.println("Hii i'm method");
+        System.out.println("Hii from method");
     }
     public static void main(String[] args) {
         methodsJava obj = new methodsJava();
-        System.out.println("The sum is "+sum(2,5));
+        // System.out.println("The sum is "+sum(2,5));
+        System.out.println("Hii from main");
         obj.printS();
     }
 }
